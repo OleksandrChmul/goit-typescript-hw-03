c189ce83f9c070435bd58f543d3cda7586244c3b
Oleksandr Chmul <al.chmul@gmail.com>
2024-04-14 23:38:10 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
-const key = new Key();
+class Key {
+	private signature: number
+	constructor() {
+		this.signature = Math.floor(Math.random() * 1000000000)
+	}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+	getSignature(): number {
+		return this.signature
+	}
+}
 
-house.openDoor(person.getKey());
+class Person {
+	constructor(private key: Key) {}
 
-house.comeIn(person);
+	getKey(): Key {
+		return this.key
+	}
+}
 
+abstract class House {
+	protected door: boolean = false
+	protected tenants: Person[] = []
 
-export {};
+	constructor(protected key: Key) {}
+
+	public comeIn(person: Person): void {
+		if (this.door) this.tenants.push(person)
+		else console.log('First you have to open the door!')
+	}
+
+	public abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+	public openDoor(key: Key): void {
+		if (this.door) return // door is already open
+
+		if (key.getSignature() === this.key.getSignature()) this.door = true
+		else console.log('Sorry, this is invalid key!')
+	}
+}
+
+const key = new Key()
+
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+export {}
